f3f9abd40728bfda7f6909a6d300d786280a3f6f
Jorden Monroe <130192394+monroe2193@users.noreply.github.com>
2024-01-26 13:08:39 -0500

    changing file names, deleting routes

---
+++ application.py
@@ -0,0 +1,35 @@
+
+#import flask class
+from flask import Flask, render_template, url_for
+
+# set an instance of the Flask class application
+app = Flask(__name__)
+#to run the above app in your local server type $env:FLASK_APP="application_name.py"
+#then type flask run
+#endpoints and functions behind them
+
+
+posts = [
+    {
+        'author':'Corey shafer',
+        'title': 'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'November 7, 2023'
+    },
+    {
+        'author':'Jorden Monroe',
+        'title': 'Blog post 2',
+        'content': 'second post content',
+        'date_posted': 'November 8, 2023'
+    }
+]
+
+
+@app.route("/")
+def PowerBI():
+    return render_template('powerbi.html', title="Power BI")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
